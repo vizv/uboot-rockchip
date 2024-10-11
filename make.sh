2f5d64def9d6ae17d05ecb275a0cbb60c3d52719
Jensen Huang <jensenhuang@friendlyarm.com>
2024-10-11 17:51:57 +0800

    scripts: Switch to more portable python3
    Signed-off-by: Jensen Huang <jensenhuang@friendlyarm.com>

--- make.sh
+++ make.sh
@@ -737,8 +737,8 @@ function pack_fit_image()
 		echo "ERROR: No 'dtc', please: apt-get install device-tree-compiler"
 		exit 1
 	elif [ "${ARM64_TRUSTZONE}" == "y" ]; then
-		if ! which python2 >/dev/null 2>&1 ; then
-			echo "ERROR: No python2"
+		if ! which python3 >/dev/null 2>&1 ; then
+			echo "ERROR: No python3"
 			exit 1
 		fi
 	fi
@@ -804,7 +804,7 @@ select_ini_file
 handle_args_late
 sub_commands
 clean_files
-make PYTHON=python2 ${ARG_SPL_FWVER} ${ARG_FWVER} CROSS_COMPILE=${TOOLCHAIN} all --jobs=${JOB}
+make PYTHON=python3 ${ARG_SPL_FWVER} ${ARG_FWVER} CROSS_COMPILE=${TOOLCHAIN} all --jobs=${JOB}
 pack_images
 finish
 echo ${TOOLCHAIN}
